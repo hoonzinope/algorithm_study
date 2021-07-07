9b8e726a011741cc6a58e7f664f89126c3d46fb6
hoonzi <chap100@naver.com>
2021-07-08 08:57:48 +0900

    Update reverse-integer.py
    불필요 line 정리

--- reverse-integer/reverse-integer.py
+++ reverse-integer/reverse-integer.py
@@ -1,9 +1,6 @@
 class Solution:
     def reverse(self, x: int) -> int:
         
-        if x > 2**31-1 or x < -2**31:
-            return 0
-        
         n_str = [n for n in str(x).replace("-","")]
         n_str.reverse()
         n = int("".join(n_str))
