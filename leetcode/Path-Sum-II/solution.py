d8670e115054489faff403a8ee6e2d3993007f20
hoonzinope <hoonzinope@gmail.com>
2022-01-24 16:44:15 +0900

    leetcode 알고리즘 문제풀이 / 2022-01-24

---
+++ leetcode/Path-Sum-II/solution.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, Node, targetSum):
+        if Node == None:
+            return None
+        else:
+            value_list = []
+            if Node.left != None:
+                left_list = self.dfs(Node.left, targetSum - Node.val)
+                if left_list != None:
+                    value_list.extend(left_list)
+            if Node.right != None:
+                right_list = self.dfs(Node.right, targetSum - Node.val)
+                if right_list != None:
+                    value_list.extend(self.dfs(Node.right, targetSum - Node.val))
+            
+            if len(value_list) == 0:
+                if targetSum == Node.val and Node.left == None and Node.right == None:
+                    return [[Node.val]]
+                else:
+                    return None
+            else:
+                for value in value_list:
+                    if targetSum == (Node.val + sum(value)):
+                        value.append(Node.val)
+                return value_list
+            
+    
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        
+        result_list = self.dfs(root, targetSum)
+        answer = []
+        if result_list != None:
+            for result in result_list:
+                if len(result) != 0:
+                    answer.append(list(reversed(result)))
+        
+        return answer
